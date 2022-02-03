c1ac12f0466643354246fc263cf7ea788cd1c649
Alison Paico <alison.jpq@gmail.com>
2022-02-03 12:22:16 -0500

    agrege seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,13 @@
 
 puts "limpiando base de datos"
 User.destroy_all if Rails.env.development?
+Course.destroy_all if Rails.env.development?
 
 User.create(email:  "carolina@gmail.com", password: "Carolina123", password_confirmation: "Carolina123")
 User.create(email:  "paola@gmail.com", password: "Paola123", password_confirmation: "Paola123")
 User.create(email:  "daniel@gmail.com", password: "Daniel123", password_confirmation: "Daniel123")
+
+Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea una Landing Page para un perfil profesional")
+Course.create(description: "Aprende a crear tus propias páginas" , name: "Lanzamiento de una web")
+Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea un e-commerce para tu marca")
+Course.create(description: "Aprende a crear tus propias páginas" , name: "Liderazgo Digital Growth")
