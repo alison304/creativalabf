982e0fb94d483a85fd69285cc2a7326d2678a7db
Midory Flores <midory.fa@gmail.com>
2022-02-04 13:08:12 -0500

    cambios

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,3 +13,24 @@ User.destroy_all if Rails.env.development?
 User.create(email:  "carolina@gmail.com", password: "Carolina123", password_confirmation: "Carolina123")
 User.create(email:  "paola@gmail.com", password: "Paola123", password_confirmation: "Paola123")
 User.create(email:  "daniel@gmail.com", password: "Daniel123", password_confirmation: "Daniel123")
+
+Course.create(description: "Curso basico de HTML", name: "HTML")
+Course.create(description: "Curso basico de JavaScript", name: "JavaScript")
+Course.create(description: "Curso basico de CSS", name: "CSS")
+
+Detail.create(name: "Etiquetas basicas", content: "Aprende a manejar las etiquetas basicas que te permitiran aprender las estructuras basicas de un archivo HTML", course_id: 1)
+Detail.create(name: "Variables en JavaScript", content: "Aprende como funciona la asignacion de Variables en JavaScript", course_id: 2)
+Detail.create(name: "Dale un diseño a tu pagina", content: "CSS te permite darle estilo a todo lo que has creado en HTML", course_id: 3)
+
+Detail.create(name: "Dale un diseño a tu pagina", content: "CSS te permite darle estilo a todo lo que has creado en HTML", course_id: 3)
+Detail.create(name: "Dale un diseño a tu pagina", content: "CSS te permite darle estilo a todo lo que has creado en HTML", course_id: 3)
+Detail.create(name: "Dale un diseño a tu pagina", content: "CSS te permite darle estilo a todo lo que has creado en HTML", course_id: 3)
+
+
+Exam.create(question: "La etiqueta <p></p> sirve para?", answer_solution: "a", detail_id: 1, answerA: "crea un nuevo parrafo")
+Exam.create(question: "What does HTML stand for?", answer_solution: "c", detail_id: 1, answerB: "colocar una imagen")
+Exam.create(question: "What does HTML stand for?", answer_solution: "b", detail_id: 1, answerC: "crear un formulario")
+
+Exam.create(question: "La etiqueta <p></p> sirve para?", answer_solution: "a", detail_id: 1, answerA: "crea un nuevo parrafo")
+Exam.create(question: "What does HTML stand for?", answer_solution: "c", detail_id: 1, answerB: "colocar una imagen")
+Exam.create(question: "What does HTML stand for?", answer_solution: "b", detail_id: 1, answerC: "crear un formulario")
