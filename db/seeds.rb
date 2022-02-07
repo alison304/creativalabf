f38798dc9086e9e2f2e1d847dd92cffc1bdad681
Caren Pampas <carenpg15@gmail.com>
2022-02-07 12:20:11 -0500

    se agrego rutas, seed, show-detail

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,8 +8,12 @@
 
 
 puts "limpiando base de datos"
-User.destroy_all if Rails.env.development?
+
+Exam.destroy_all
+Detail.destroy_all
 Course.destroy_all
+User.destroy_all if Rails.env.development?
+
 
 puts "creando nuevas seeds"
 User.create(email:  "carolina@gmail.com", password: "Carolina123", password_confirmation: "Carolina123")
@@ -20,15 +24,15 @@ User.create(email:  "daniel@gmail.com", password: "Daniel123", password_confirma
 # Course.create(description: "Curso basico de JavaScript", name: "JavaScript")
 # Course.create(description: "Curso basico de CSS", name: "CSS")
 
-Detail.create(name: "Etiquetas basicas", content: "Aprende a manejar las etiquetas basicas que te permitiran aprender las estructuras basicas de un archivo HTML", course_id: 1)
-Detail.create(name: "Variables en JavaScript", content: "Aprende como funciona la asignacion de Variables en JavaScript", course_id: 2)
-Detail.create(name: "Dale un diseño a tu pagina", content: "CSS te permite darle estilo a todo lo que has creado en HTML", course_id: 3)
+course1 = Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea una Landing Page para un perfil profesional")
+course2 = Course.create(description: "Aprende a crear tus propias páginas" , name: "Lanzamiento de una web")
+course3 = Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea un e-commerce para tu marca")
+course4 = Course.create(description: "Aprende a crear tus propias páginas" , name: "Liderazgo Digital Growth")
 
-Exam.create(question: "La etiqueta <p></p> sirve para?", answer_solution: "a", detail_id: 1, answerA: "crea un nuevo parrafo")
-Exam.create(question: "What does HTML stand for?", answer_solution: "c", detail_id: 1, answerB: "colocar una imagen")
-Exam.create(question: "What does HTML stand for?", answer_solution: "b", detail_id: 1, answerC: "crear un formulario")
+detail1 = Detail.create(name: "Etiquetas basicas", content: "https://www.youtube.com/embed/D-3Qg4pf-E4", course: course1)
+Detail.create(name: "Variables en JavaScript", content: "https://www.youtube.com/embed/LRfaU7bdnPY", course: course1)
+Detail.create(name: "Dale un diseño a tu pagina", content: "https://www.youtube.com/embed/9cGqmcWJ2JA", course: course1)
 
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea una Landing Page para un perfil profesional")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Lanzamiento de una web")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea un e-commerce para tu marca")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Liderazgo Digital Growth")
+Exam.create(question: "La etiqueta <p></p> sirve para?", answer_solution: "a", detail: detail1, answerA: "crea un nuevo parrafo")
+Exam.create(question: "What does HTML stand for?", answer_solution: "c", detail: detail1, answerB: "colocar una imagen")
+Exam.create(question: "What does HTML stand for?", answer_solution: "b", detail: detail1, answerC: "crear un formulario")
