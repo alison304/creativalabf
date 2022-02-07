9d39272be361924436f6f579446557e0df225ff8
Alison Paico <alison.jpq@gmail.com>
2022-02-07 17:40:47 -0500

    av

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,10 +24,30 @@ User.create(email:  "daniel@gmail.com", password: "Daniel123", password_confirma
 # Course.create(description: "Curso basico de JavaScript", name: "JavaScript")
 # Course.create(description: "Curso basico de CSS", name: "CSS")
 
-course1 = Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea una Landing Page para un perfil profesional")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Lanzamiento de una web")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Crea un e-commerce para tu marca")
-Course.create(description: "Aprende a crear tus propias páginas" , name: "Liderazgo Digital Growth")
+course1 = Course.create(description: "¿Sabías que, siendo developer, diseñador, marketero, entre otros, puedes crear tu propio landing page?:cara_pensativa::lupa:
+Aprende a crear tu landing page desde cero
+En Creativa diseñamos un programa de 1 mes que te entrenará para crear tu propia web profesional en Webflow :grito:
+:ordenador: Sesiones en vivo y grabadas.
+:profesor: Tendrás el acompañamiento de expertos durante todo el programa.
+:trofeo: Tu aprendizaje será basado en retos y crearás webs durante el programa." , name: "Crea una Landing Page para un perfil profesional")
+Course.create(description: "Aprende a lanzar tu web con éxito al mercado
+¿Sabías que, siendo developer, diseñador, marketero, entre otros, puedes crear tu propio landing page?:cara_pensativa::lupa:
+En Creativa diseñamos un programa de 1 mes que te entrenará para lanzar con dominio y hosting:grito:
+:ordenador: Online en vivo.
+:profesor: Tendrás el acompañamiento de expertos durante todo el programa.
+:trofeo: Tu aprendizaje será basado en retos y crearás webs durante el programa." , name: "Lanzamiento de una web")
+Course.create(description: "Curso e-commerce: Cómo montar tu tienda online con Shopify
+¿Sabías que, siendo developer, diseñador, marketero, entre otros, puedes crear tu propia tienda e-commerce?:cara_pensativa::lupa:
+En Creativa diseñamos un programa de 1 mes que te entrenará para crear tu tienda desde cero:grito:
+:ordenador: Sesiones en vivo y grabadas
+:profesor: Tendrás el acompañamiento de expertos durante todo el programa.
+:trofeo: Tu aprendizaje será basado en retos y crearás webs durante el programa." , name: "Crea un e-commerce para tu marca")
+Course.create(description: "En este curso de Liderazgo a través del servicio, aprenderás a reconocer tus fortalezas como líder y aquellas características propias que te limitan y te impiden liberar todo tu potencial de impacto e influencia en los otros y tu entorno.
+De la mano de Jose Luis Pinheira, tu profesor en este curso de Liderazgo a través del servicio, entenderás qué es el thought leadership organization, cuáles son sus características y qué necesitas para construir una organización basada en el thought leadership.
+En Creativa diseñamos un programa de 1 mes que te entrenará para desarrollar habilidades de liderazgo growth:grito:
+:ordenador: Sesiones en vivo y grabadas
+:profesor: Tendrás el acompañamiento de expertos durante todo el programa.
+:trofeo: Tu aprendizaje será basado en retos y crearás webs durante el programa." , name: "Liderazgo Digital Growth")
 
 detail1 = Detail.create!(name: "Etiquetas basicas", content: "https://www.youtube.com/embed/D-3Qg4pf-E4", course: course1)
 Detail.create!(name: "Variables en JavaScript", content: "https://www.youtube.com/embed/LRfaU7bdnPY", course: course1)
